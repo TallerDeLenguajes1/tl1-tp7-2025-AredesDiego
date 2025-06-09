10a22ad754b1760037e0773012323aa6abd40db3
Diego Aredes <aredesdiegofabian@gmail.com>
2025-06-09 11:49:57 -0300

    Terminando Punto 2

--- SusiPersonal/Empleado.cs
+++ SusiPersonal/Empleado.cs
@@ -1,17 +1,76 @@
-namespace Empleado.logica
-{
-    class Empleado
-    {
-        string nombre;
-        string apellido;
-        DateTime fecha_nacimiento;
-        char estado_civil;
-        double sueldo_basico;
+using System.Security.Cryptography;
 
-        public Empleado(DateTime fecha)
+namespace LogicaEmpleado
+{
+    public class Empleado
+    {
+        public Empleado(string apellido, string nombre, DateTime fecha_n, DateTime fecha_ingreso)
         {
-            this.fecha_nacimiento = fecha;
+            this.Apellido = apellido;
+            this.Nombre = nombre;
+            this.FechaNacimiento = fecha_n;
+            this.FechaIngresoEmpresa = fecha_ingreso;
+        }
+        public enum Cargos
+        {
+            Auxiliar,
+            Administrativo,
+            Ingeniero,
+            Especialista,
+            Investigador
         }
 
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        private DateTime FechaNacimiento { get; }
+        private DateTime FechaIngresoEmpresa { get; }
+
+        public DateTime GetFechaNacimiento()
+        {
+            return FechaNacimiento;
+        }
+        public DateTime GetFechaIngresoEmpresa()
+        {
+            return FechaIngresoEmpresa;
+        }
+        public double SueldoBasico { get; set; }
+        public char EstadoCivil { get; set; }
+        public Cargos Cargo { get; set; }
+
+        public int calculo_tiempo(DateTime fecha_inicio)
+        {
+            return DateTime.Now.Year - fecha_inicio.Year;
+        }
+        public int jubilacion(DateTime fecha_inicio)
+        {
+            int edad = calculo_tiempo(fecha_inicio);
+            if (edad <= 65)
+                 return  65 - edad;
+            return 0;
+        }
+        public double adicional(DateTime fecha_inicio, double sueldo_basico)
+        {
+            double adicional = 0;
+
+            //Calculos por años de antiguedad
+            if (jubilacion(fecha_inicio) <= 20)
+                adicional = sueldo_basico + 0.2 * sueldo_basico;
+            else
+                adicional = sueldo_basico + 0.25 * sueldo_basico;
+
+            //Calculos por especialidad
+            if (Cargo == Cargos.Ingeniero || Cargo == Cargos.Especialista)
+                adicional += sueldo_basico + 0.5;
+
+            //Calculos por estado civil
+            if (EstadoCivil == 'C')
+                adicional += 150000;
+
+            return adicional;
+        }
+        public double calcular_salario(double sueldo_basico, double adicional)
+        {
+            return sueldo_basico + adicional;
+        }
     }   
 }
--- SusiPersonal/Program.cs
+++ SusiPersonal/Program.cs
@@ -1,9 +1,56 @@
-﻿using System.Diagnostics;
+﻿using System;
+using LogicaEmpleado;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        double total_salarios = 0;
+        Empleado[] arreglo = new Empleado[3];
+
+        arreglo[0] = new Empleado("Gonzalez", "Laura", new DateTime(1980, 5, 20), new DateTime(2005, 6, 1));
+        arreglo[0].SueldoBasico = 300000;
+        arreglo[0].EstadoCivil = 'C';
+        arreglo[0].Cargo = Empleado.Cargos.Ingeniero;
+
+        arreglo[1] = new Empleado("Martinez", "Carlos", new DateTime(1975, 3, 10), new DateTime(2010, 4, 15));
+        arreglo[1].SueldoBasico = 280000;
+        arreglo[1].EstadoCivil = 'S';
+        arreglo[1].Cargo = Empleado.Cargos.Administrativo;
+
+        arreglo[2] = new Empleado("Lopez", "Ana", new DateTime(1990, 7, 12), new DateTime(2018, 1, 5));
+        arreglo[2].SueldoBasico = 250000;
+        arreglo[2].EstadoCivil = 'C';
+        arreglo[2].Cargo = Empleado.Cargos.Especialista;
+
+        // Buscar el empleado más próximo a jubilarse
+        int indiceMin = 0;
+        int minAniosJubilacion = arreglo[0].jubilacion(arreglo[0].GetFechaNacimiento());
+
+        for (int i = 1; i < arreglo.Length; i++)
+        {
+            int anios = arreglo[i].jubilacion(arreglo[i].GetFechaNacimiento());
+            if (anios < minAniosJubilacion)
+            {
+                minAniosJubilacion = anios;
+                indiceMin = i;
+            }
+        }
+
+        // Mostrar solo ese empleado
+        Empleado emp = arreglo[indiceMin];
+        double adicional = emp.adicional(emp.GetFechaIngresoEmpresa(), emp.SueldoBasico);
+        double salarioFinal = emp.calcular_salario(emp.SueldoBasico, adicional);
+        total_salarios += salarioFinal;
+
+        Console.WriteLine("Empleado más próximo a su jubilación:");
+        Console.WriteLine($"Nombre: {emp.Nombre} {emp.Apellido}");
+        Console.WriteLine($"Cargo: {emp.Cargo}");
+        Console.WriteLine($"Sueldo Básico: {emp.SueldoBasico}");
+        Console.WriteLine($"Adicional: {adicional}");
+        Console.WriteLine($"Salario Final: {salarioFinal}");
+        Console.WriteLine($"Años para jubilarse: {minAniosJubilacion}");
+
+        Console.WriteLine($"TOTAL SALARIOS{total_salarios}");
     }
 }
